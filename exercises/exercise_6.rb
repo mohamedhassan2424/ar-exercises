000a39591eb69c0388547e766c16cd53472218af
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-17 19:45:14 +0000

    completed the one-to-many relationship created alot of employes for the first store id

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1 = Store.find_by(id:'1')
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mohamed", last_name: "Hassan", hourly_rate: 60)
+@store1.employees.create(first_name: "Frank ", last_name: "Cenatra", hourly_rate: 60)
+@store1.employees.create(first_name: "Lebron", last_name: "James", hourly_rate: 60)
+@store1.employees.create(first_name: "Kobe", last_name: "Brynat", hourly_rate: 60)
